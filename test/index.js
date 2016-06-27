8549c5e06a317edc42813514721f684cc4a80353
Matt Edelman <medelman@paypal.com>
2016-06-27 16:13:56 -0700

    fix for DoS: https://nodesecurity.io/advisories/118

--- test/index.js
+++ test/index.js
@@ -25,108 +25,106 @@ var test = require('tap').test,
   testutil = require('./util'),
   path = require('path');
 
-test('construx', function (t) {
+test('returns a middleware chain using an empty config', function(t) {
+  var app = testutil.createApp();
 
-    t.test('returns a middleware chain using an empty config', function (t) {
-        var app = testutil.createApp();
-
-        request(app)
-          .get('/')
-          .expect(200, function (err) {
-              t.error(err);
-              testutil.cleanUp(t.end.bind(t));
-          });
-
+  request(app)
+    .get('/')
+    .expect(200, function(err) {
+      t.error(err);
+      testutil.cleanUp(t.end);
     });
 
-    t.test('returns a middleware chain using a non-empty config', function (t) {
-        var app = testutil.createApp({
-            copier: {
-                module: path.resolve(__dirname, 'plugins/copier'),
-                files: '**/*'
-            }
+});
+
+test('returns a middleware chain using a non-empty config', function(t) {
+  var app = testutil.createApp({
+    copier: {
+      module: path.resolve(__dirname, 'plugins/copier'),
+      files: '**/*'
+    }
+  });
+
+  request(app)
+    .get('/')
+    .expect(200, function(err) {
+      t.error(err);
+      testutil.cleanUp(t.end);
+    });
+});
+test('will exercise middleware for 200 result', function(t) {
+var app = testutil.createApp({
+  copier: {
+    module: path.resolve(__dirname, 'plugins/copier'),
+    files: '**/*'
+  }
+});
+
+request(app)
+  .get('/foo/')
+  .expect(/Hello/)
+  .expect(200, function(err) {
+    t.error(err);
+    testutil.cleanUp(t.end);
+  });
+});
+});
+test('will exercise middleware for 404 result', function(t) {
+  var app = testutil.createApp({
+    copier: {
+      module: path.resolve(__dirname, 'plugins/copier'),
+      files: '**/*'
+    }
+  });
+
+  request(app)
+    .get('/foo/bar')
+    .expect(404, function(err) {
+      t.error(err);
+      request(app).post('/foo/bar')
+        .expect(404, function(err) {
+          t.error(err);
+          testutil.cleanUp(t.end);
         });
-
-        request(app)
-          .get('/')
-          .expect(200, function (err) {
-              t.error(err);
-              testutil.cleanUp(t.end.bind(t));
-          });
     });
-    t.test('will exercise middleware for 200 result', function (t) {
-        var app = testutil.createApp({
-            copier: {
-                module: path.resolve(__dirname, 'plugins/copier'),
-                files: '**/*'
-            }
-        });
-
-        request(app)
-          .get('/foo/')
-          .expect(/Hello/)
-          .expect(200, function (err) {
-              t.error(err);
-              testutil.cleanUp(t.end.bind(t));
-          });
+  t.test('will honor the context.skipRead option', function(t) {
+    var app = testutil.createApp({
+      copier: {
+        module: path.resolve(__dirname, 'plugins/copier'),
+        files: '**/*',
+        precompile: function(options, cb) {
+          options.skipRead = true;
+          cb(null, options);
+        }
+      }
     });
-    t.test('will exercise middleware for 404 result', function (t) {
-        var app = testutil.createApp({
-            copier: {
-                module: path.resolve(__dirname, 'plugins/copier'),
-                files: '**/*'
-            }
-        });
 
-        request(app)
-          .get('/foo/bar')
-          .expect(404, function (err) {
-              t.error(err);
-              request(app).post('/foo/bar')
-                .expect(404, function (err) {
-                    t.error(err);
-                    testutil.cleanUp(t.end.bind(t));
-                });
-          });
+    request(app)
+      .get('/foo/')
+      .expect(' ')
+      .expect(200, function(err) {
+        t.error(err);
+        testutil.cleanUp(t.end.bind(t));
+      });
+  });
+  t.test('will honor the context.srcPath option', function(t) {
+    var app = testutil.createApp({
+      copier: {
+        module: path.resolve(__dirname, 'plugins/copier'),
+        files: '**/*',
+        precompile: function(options, cb) {
+          options.srcPath = path.resolve(__dirname, 'fixtures/public/bar/index.html');
+          cb(null, options);
+        }
+      }
     });
-    t.test('will honor the context.skipRead option', function (t) {
-        var app = testutil.createApp({
-            copier: {
-                module: path.resolve(__dirname, 'plugins/copier'),
-                files: '**/*',
-                precompile: function (options, cb) {
-                    options.skipRead = true;
-                    cb(null, options);
-                }
-            }
-        });
 
-        request(app)
-          .get('/foo/')
-          .expect(' ')
-          .expect(200, function (err) {
-              t.error(err);
-              testutil.cleanUp(t.end.bind(t));
-          });
-    });
-    t.test('will honor the context.srcPath option', function (t) {
-        var app = testutil.createApp({
-            copier: {
-                module: path.resolve(__dirname, 'plugins/copier'),
-                files: '**/*',
-                precompile: function (options, cb) {
-                    options.srcPath = path.resolve(__dirname, 'fixtures/public/bar/index.html');
-                    cb(null, options);
-                }
-            }
-        });
-
-        request(app)
-          .get('/foo/')
-          .expect(/Goodbye/)
-          .expect(200, function (err) {
-              t.error(err);
-              testutil.cleanUp(t.end.bind(t));
-          });
-    });
+    request(app)
+      .get('/foo/')
+      .expect(/Goodbye/)
+      .expect(200, function(err) {
+        t.error(err);
+        testutil.cleanUp(t.end.bind(t));
+      });
+  });
 });
